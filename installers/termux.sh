5842d213d25acfc23e556069c44c75581e050aa1
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-11-02 06:32:05 -0700

    Add files via upload

---
+++ installers/termux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Updating package list..."
+pkg update
+
+echo "Installing Python and pip..."
+pkg install -y python3 python3-pip
+
+echo "Installing required Python packages..."
+pip3 install ecdsa requests
+pkg install python3-cryptography
+
+echo "Verifying installation..."
+python3 -c "import hashlib; import base58; import binascii; from ecdsa import SigningKey, SECP256k1; import json; import base64; import getpass; import requests; from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes; from cryptography.hazmat.backends import default_backend; from cryptography.hazmat.primitives import padding; import secrets"
+
+if [ $? -eq 0 ]; then
+    echo "All dependencies installed successfully."
+else
+    echo "There was an error in installation."
+fi
