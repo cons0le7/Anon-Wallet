a32fb2a67ed480b7455579a233135a87f6f580ed
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-11-15 19:00:47 +0000

    Add files via upload

--- installers/install.sh
+++ installers/install.sh
@@ -10,10 +10,13 @@ echo "Installing Python and pip..."
 sudo apt-get install -y python3 python3-pip
 
 echo "Installing required Python packages..."
-pip3 install ecdsa cryptography requests
+sudo apt install python3-ecdsa 
+sudo apt install python3-cryptography 
+sudo apt install python3-requests 
+sudo apt install base58 -y 
 
 echo "Verifying installation..."
-python3 -c "import hashlib; import base58; import binascii; from ecdsa import SigningKey, SECP256k1; import json; import base64; import getpass; import requests; from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes; from cryptography.hazmat.backends import default_backend; from cryptography.hazmat.primitives import padding; import secrets"
+sudo python3 -c "import hashlib; import base58; import binascii; from ecdsa import SigningKey, SECP256k1; import json; import base64; import getpass; import requests; from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes; from cryptography.hazmat.backends import default_backend; from cryptography.hazmat.primitives import padding; import secrets"
 
 if [ $? -eq 0 ]; then
     echo "All dependencies installed successfully."
